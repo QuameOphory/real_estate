a178e0946e4f702ff1b01292800ea8f48ed554c0
QuameOphory <quameophory@yahoo.com>
2022-09-12 21:09:30 +0000

    create html pages

---
+++ listings/urls.py
@@ -0,0 +1,10 @@
+from unicodedata import name
+from django.urls import path
+from . import views
+import listings
+
+urlpatterns = [
+    path('', views.index, name='listings'),
+    path('<int:listing_id>/', views.listing, name='listing'),
+    path('search', views.search, name='search'),
+]
