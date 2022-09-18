ba6d1a22540d44057a29f4dd53ccd288f2e58881
QuameOphory <quameophory@yahoo.com>
2022-09-18 14:53:49 +0000

    add listing page

--- realtors/admin.py
+++ realtors/admin.py
@@ -7,7 +7,8 @@ from .models import Realtor
 class RealtorAdmin(admin.ModelAdmin):
     '''Admin View for Realtor'''
 
-    list_display = ('name', 'phone', 'email', 'hire_date',)
+    list_display = ('name', 'phone', 'email', 'hire_date', 'is_mvp',)
     list_filter = ('name',)
+    list_editable = ('is_mvp',)
     search_fields = ('name',)
     list_per_page = 25
