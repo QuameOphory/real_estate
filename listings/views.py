da828444d07f95b5f14c8b9ecb1bce2c49d00f3e
QuameOphory <quameophory@yahoo.com>
2022-09-21 09:28:03 +0000

    add paginations to listings

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
@@ -17,8 +17,12 @@ def index(request):
     return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-    listing = Listing.objects.get(pk=listing_id)
-    return render(request, 'listings/listing.html')
+    #listing = Listing.objects.get(pk=listing_id)
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
