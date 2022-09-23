a406ae321527ea1e93aca59089b6d786431c9d8f
QuameOphory <quameophory@yahoo.com>
2022-09-23 12:40:50 -0400

    change database configuration

--- restate/settings.py
+++ restate/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'restate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'PASSWORD': 'thriller2',
+        'USER': 'postgres',
     }
 }
 
